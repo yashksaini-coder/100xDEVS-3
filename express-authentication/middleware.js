40f5a20b1c246198ead5c0326d1f815ceb07bb10
yashksaini-coder <ys3853428@gmail.com>
2025-04-21 23:53:21 +0530

    (auth):Middleware function

---
+++ express-authentication/middleware.js
@@ -0,0 +1,28 @@
+function auth(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401).send({
+                    message: "Unauthorized"
+                })
+            } else {
+                req.user = decoded;
+                next();
+            }
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+}
+
+app.get("/me", auth, (req, res) => {
+    const user = req.user;
+
+    res.send({
+        username: user.username
+    })
+})
