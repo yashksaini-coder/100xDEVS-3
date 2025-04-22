9fa05f3c6a99f64c3adbcfe18c22b955c1ef45da
yashksaini-coder <ys3853428@gmail.com>
2025-04-23 00:35:10 +0530

    (clean): Delete previous weeks

--- nextjs-backend/app/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-//@ts-ignore
-const prisma = globalThis.prisma ?? new PrismaClient();
-
-//@ts-ignore
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
-export default prisma;
