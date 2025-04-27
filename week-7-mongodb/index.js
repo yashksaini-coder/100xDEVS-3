02ad96f1045d7e901e78845a481e44c912b217d4
yashksaini-coder <ys3853428@gmail.com>
2025-04-28 00:56:22 +0530

    (fix & refractor): signin & todo routes to include JWT and userId extraction

--- week-7-mongodb/index.js
+++ week-7-mongodb/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const auth = require("./auth.js").auth; // Importing the auth middleware
+const mongoose = require("mongoose");
+const {user, todo} = require("./db.js");
+
 const app = express();
 
 // connecting to the database
-const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin:root@cluster0.7kmvq.mongodb.net")
-
-// importing the models from db.js
-const { user, todo } = require("./db.js");
+mongoose.connect("mongodb+srv://admin:root@main.cqd4hyx.mongodb.net/test")
 const JWT_SECRET = "c2VjcmV0Cg=="; // Base64 encoded string of "secret"
 
 
@@ -33,14 +34,68 @@ app.post("/signup", async function(req, res) {
     });
 });
 
-app.post("/signin", function(req, res) {
-    const username = req.body.username;
+app.post("/signin", async function(req, res) {
+    const email = req.body.email;
     const password = req.body.password;
+
+    const foundUser = await user.findOne({
+        email: email,
+        password: password
+    });
+
+    console.log(foundUser);
+    // If the user is found, create a JWT token and send it back to the client
+
+    if(foundUser) {
+        const token = jwt.sign({
+            id: foundUser._id.toString(),
+        }, JWT_SECRET);
+        res.json({
+            message: "User signed in successfully",
+            token: token
+        });
+    } else {
+        res.status(401).json({
+            message: "Invalid username or password"
+        });
+    }
 });
 
-app.post("/todo", function(req, res) { 
+app.post("/todo", auth, async function(req, res) {
+    const userId = req.userId; // Extracted from the JWT token
+    const title = req.body.title;
+    const description = req.body.description;
 
+    await todo.create({
+        title: title,
+        description: description,
+        userId: userId
+    });
+
+    res.json({
+        message: "Todo created successfully",
+        userId: userId,
+        todo: {
+            title: title,
+            description: description
+        }
+    });
 });
 
-app.get("/todos", function(req, res) { 
-}); app.listen(3000);
+
+app.get("/todos", auth, async function(req, res) {
+    const userId = req.userId; // Extracted from the JWT token
+    
+    const todos = await todo.find({ 
+        userId: userId
+    });
+
+    res.json({
+        todos: todos
+    });
+}); 
+
+
+
+
+app.listen(3000);
