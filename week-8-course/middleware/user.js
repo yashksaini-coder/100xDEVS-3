8fd7e3961e87fa397081266fd3c106c19813213c
yashksaini-coder <ys3853428@gmail.com>
2025-05-05 00:46:13 +0530

    feat: add admin and user middleware for JWT authentication

---
+++ week-8-course/middleware/user.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+
+function userMiddleware (req,res,next) {
+    const token = req.headers.token;
+
+    if(!token){
+        res.status(401).json({messsage:"Authorization token missing"})
+    }
+
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET);
+        if (decoded) {
+            req.userId = decoded.id;
+            next()
+        } else {
+            res.status(403).json({
+                message: "You are not signed in"
+            })
+        }
+    } catch (error) {
+        res.status(401).json({message:"Invalid token"})
+    }
+}
+
+module.exports = {
+    userMiddleware: userMiddleware
+}
